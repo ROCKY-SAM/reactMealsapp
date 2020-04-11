ba6b07724a91f34ac3affb20bf96312b02010795
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-04-12 00:56:56 +0530

    add fonts

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const CategoriesMealsScreen = props =>{
+    return (
+        <View style={styles.screen} >
+            <Text>The CategoriesMealsScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+screen:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+}
+});
+export default CategoriesMealsScreen;
