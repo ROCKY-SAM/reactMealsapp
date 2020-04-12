13d22223146621f7b98766dbb4ce7ae276a1cf75
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-04-13 00:50:30 +0530

    Configuring the Header with Navigation Options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet,FlatList,TouchableOpacity,Platform } from 'react-native';
+import { CATEGORIES} from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = props => {
+  
+const renderGridItem=(itemData)=>{
+  return (<TouchableOpacity  style={styles.gridItem} onPress={()=>{
+    props.navigation.navigate({routeName:'CategoryMeals'});
+  }}>
+   <View>
+    <Text>{itemData.item.title}</Text>
+    </View></TouchableOpacity>
+    );
+};
+
+
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button title="Go to Meals!" onPress={() => {
-          props.navigation.navigate({routeName: 'CategoryMeals'});
-      }} />
-    </View>
+    <FlatList 
+    keyExtractor={(item,index) =>item.id}
+    data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle:{
+    backgroundColor:Platform.OS === 'android' ? Colors.primaryColor :''
+  },
+  headerTintColor:Platform.OS === 'android' ? 'white' :Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
@@ -17,6 +36,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },gridItem:{
+    flex:1,
+    margin:15,
+    height:150
   }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -10,6 +10,10 @@ const CategoryMealScreen = props => {
               routeName: 'MealDetail'
           });
       }} />
+
+      <Button title="Go back" onPress={()=>{
+        props.navigation.pop();
+      }}/>
     </View>
   );
 };
