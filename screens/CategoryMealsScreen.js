b4befdec4e902a7740047200aaa8e53e3d5e03e7
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-04-13 23:19:26 +0530

    Passing Data to the Meal Detail Screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
- 
-import {CATEGORIES,MEALS} from '../data/dummy-data';
+
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
 const CategoryMealScreen = props => {
 
-const renderMealItem = itemData =>{
-return <MealItem title={itemData.item.title} 
-duration={itemData.item.duration} 
-complexity={itemData.item.complexity}
-affordability={itemData.item.affordability}
-image = {itemData.item.imageUrl}
-onSelectMeal={()=>{
-
-}}/>;
-};
+  const renderMealItem = itemData => {
+    return <MealItem title={itemData.item.title}
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity}
+      affordability={itemData.item.affordability}
+      image={itemData.item.imageUrl}
+      onSelectMeal={() => {
+        props.navigation.navigate({routeName:'MealDetail',
+      params:{
+        mealId:itemData.item.id
+      }});
+      }} />;
+  };
 
   const catId = props.navigation.getParam('categoryId');
 
   //const selectedCategory = CATEGORIES.find(cat =>cat.id === catId);
-const displayedMeals = MEALS.filter(meal=> meal.categoryIds.indexOf(catId) >= 0);
-  
+  const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
   return (
     <View style={styles.screen}>
-      <FlatList data={displayedMeals} keyExtractor={(item,index) => item.id} renderItem={renderMealItem} style={{width:'100%'}}/>
+      <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem} style={{ width: '100%' }} />
     </View>
   );
 };
 
-CategoryMealScreen.navigationOptions = navigationData =>{
+CategoryMealScreen.navigationOptions = navigationData => {
   const catId = navigationData.navigation.getParam('categoryId');
-  const selectedCategory = CATEGORIES.find(cat=>cat.id === catId);
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return {
-    headerTitle:selectedCategory.title
+    headerTitle: selectedCategory.title
   };
 };
 
